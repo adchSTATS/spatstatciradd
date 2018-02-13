5946564e9ed6a3302c210759ffb7d65f460bc4b8
Andreas Dyreborg Christoffersen <andreas@math.aau.dk>
2018-02-13 13:51:24 +0100

    Argument changes in d_uni_vM_mix

--- R/d_uni_vM_mix.R
+++ R/d_uni_vM_mix.R
@@ -1,7 +1,7 @@
 #' Mixture Density Function of Uniform and von Mises
 #'
 #' Density function for the mixture of the uniform and von Mises distribution.
-#' @param x A numeric vector representing angles. Is coerced to class \code{\link{circular}}.
+#' @param x An object of class \code{\link{ppc}}.
 #' @param mu Mean direction of the distribution. Is coerced to class \code{\link{circular}}.
 #' @param kappa Non-negative numeric value for the concentration parameter of the distribution.
 #' @param p_unif A numeric between 0 and 1. The probability of observing a point from the uniform distribution.
@@ -10,5 +10,7 @@
 #' @importFrom stats dunif
 #' @export
 d_uni_vM_mix <- function(x, mu, kappa, p_unif) {
-  p_unif * dunif(x, min = 0, max = 2 * pi) + (1 - p_unif) * dvonmises(x = circular(x), mu = circular(mu), kappa = kappa)
+  stopifnot(verifyclass(x, "ppc"))
+  p_unif * dunif(x$data$angs, min = 0, max = 2 * pi) +
+    (1 - p_unif) * dvonmises(x = circular(x$data$angs), mu = circular(mu), kappa = kappa)
 }
